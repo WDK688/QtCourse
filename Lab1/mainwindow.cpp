f87928c76fe5d8e0ee8c8b4c2a74d53d5ac915f2
WDK688 <1538860507@qq.com>
2025-10-17 10:29:05 +0800

    初始化

---
+++ Lab1/mainwindow.cpp
@@ -0,0 +1,76 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    connect(ui->btnNum0,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum1,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum2,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum3,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum4,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum5,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum6,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum7,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum8,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum9,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+
+    connect(ui->btnMultiple,SIGNAL(clicked()),this,SLOT(btnBinaryOperatorClicked()));
+    connect(ui->btnDivide,SIGNAL(clicked()),this,SLOT(btnBinaryOperatorClicked()));
+    connect(ui->btnMinus,SIGNAL(clicked()),this,SLOT(btnBinaryOperatorClicked()));
+    connect(ui->btnPlus,SIGNAL(clicked()),this,SLOT(btnBinaryOperatorClicked()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::btnNumClicked()
+{
+    QString digit =qobject_cast<QPushButton*>(sender())->text();
+    if(digit == "0" && operand=="0")
+        digit = "";
+    if(operand == "0" && digit!="0")
+      operand = "";
+
+    operand += digit;
+    ui->display->setText(operand);
+}
+
+void MainWindow::btnBinaryOperatorClicked()
+{
+
+}
+
+void MainWindow::on_btnPeriod_clicked()
+{
+    if(!operand.contains(".")){
+        operand += qobject_cast<QPushButton*>(sender())->text();
+        ui->display->setText(operand);
+    }
+}
+
+
+void MainWindow::on_btnDel_clicked()
+{
+    operand=operand.left(operand.length() - 1);
+    ui->display->setText(operand);
+}
+
+
+void MainWindow::on_btnClear_clicked()
+{
+    operand.clear();
+    ui->display->setText(operand);
+}
+
+
+void MainWindow::on_btnEqual_clicked()
+{
+
+}
+
---
+++ Lab1/mainwindow.h
@@ -0,0 +1,39 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include<QLabel>
+#include<QStack>
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    QString operand;
+    QStack<QString>operands;
+
+private slots:
+    void btnNumClicked();
+    void btnBinaryOperatorClicked();
+
+    void on_btnPeriod_clicked();
+
+    void on_btnDel_clicked();
+
+    void on_btnClear_2_clicked();
+
+    void on_btnClear_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
